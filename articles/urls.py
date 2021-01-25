113acbf03091cebf57f4b8026900f535aaac1aa1
masriham <masriham@hhh.com>
2021-01-26 01:18:42 +0200

    save all

--- articles/urls.py
+++ articles/urls.py
@@ -4,8 +4,12 @@ from blogs import settings
 from django.urls import path, include
 from articles import views as article_views
 
+
+app_name = 'articles'
 urlpatterns = [
     path('', article_views.IndexPageView.as_view(), name='index'),
+    path('category_results/<str:title>/', article_views.category_results.as_view(), name='category_results'),
+    path('search', article_views.SearchView.as_view(), name='search_result'),
     
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- articles/views.py
+++ articles/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from django.contrib import messages
 from articles import models as article_views
+from django.urls import reverse, resolve
 # Create your views here.
 
 
@@ -34,8 +35,86 @@ def needed_everywhere():
     context['all_category'] = all_categories
     context['all_articles'] = all_articles
 
-    print(all_categories)
-    print(all_articles)
+ 
     
 
     return context
+
+# def category_results(request,category_title):
+#     return render(request,'articles/list_category.html')
+
+
+
+class category_results(ListView):
+
+    template_name = 'articles/list_category.html'
+    model = article_views.Article
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        context = super(category_results, self).get_context_data(**kwargs)
+        category_title = self.kwargs.get('title')
+        articles = article_views.Article.objects.filter(category__title=  category_title) 
+     
+        context ['category_title'] = category_title
+
+        paginator = Paginator(articles, self.paginate_by)
+
+        page = self.request.GET.get('page')
+
+        try:
+            articles_files = paginator.page(page)
+        except PageNotAnInteger:
+            articles_files = paginator.page(1)
+        except EmptyPage:
+            articles_files = paginator.page(paginator.num_pages)
+            
+        context['articles'] = articles_files
+        return context
+
+
+
+class SearchView(ListView):
+
+    template_name = 'articles/list_category.html'
+    model = article_views.Article
+    paginate_by = 4
+    def get_context_data(self, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        search_term = self.request.GET.get('q').strip(' */%^$#@!?')
+        articles = article_views.Article.objects.filter(Q(title__icontains=search_term) | Q(
+            author__icontains=search_term) | Q(category__title__icontains=search_term) | Q(description__icontains=search_term))
+        article_number = articles.count()
+        current_url = resolve(self.request.path_info).url_name
+
+
+        paginator = Paginator(articles, self.paginate_by)
+
+        page = self.request.GET.get('page')
+
+        try:
+            articles_files = paginator.page(page)
+        except PageNotAnInteger:
+            articles_files = paginator.page(1)
+        except EmptyPage:
+            articles_files = paginator.page(paginator.num_pages)
+            
+        
+        context = {
+            'articles': articles_files,
+            'products_number': article_number,
+            'search': search_term,
+            'url_name': current_url,
+            'search_exist': 'true',
+        }
+      
+       
+        return context
+
+    
+   
+
+    # def get_queryset(self,**kwargs):
+       
+        
+    #     return context
